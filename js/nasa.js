5945d14308a2d55bc7022e3deece15a29849d6cb
Robert Lemieux <roblem28@gmail.com>
2026-01-19 15:59:36 -0500

    Initial NASA Explorer

---
+++ js/nasa.js
@@ -0,0 +1,44 @@
+const API_KEY = window.NASA_API_KEY;
+
+function loadAPOD() {
+  const date = document.getElementById("apodDate").value;
+
+  let url =
+    "https://api.nasa.gov/planetary/apod?api_key=" + API_KEY;
+
+  if (date) {
+    url += "&date=" + date;
+  }
+
+  fetch(url)
+    .then(r => r.json())
+    .then(d => {
+      document.getElementById("output").innerHTML =
+        "<h2>" + d.title + "</h2>" +
+        "<img src='" + d.url + "'>" +
+        "<p>" + d.explanation + "</p>";
+    });
+}
+
+function loadMars() {
+  const camera = document.getElementById("camera").value;
+
+  const url =
+    "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos" +
+    "?sol=1000&camera=" + camera +
+    "&api_key=" + API_KEY;
+
+  fetch(url)
+    .then(r => r.json())
+    .then(d => {
+      if (d.photos.length === 0) {
+        document.getElementById("output").innerHTML =
+          "No photos for that camera.";
+        return;
+      }
+
+      document.getElementById("output").innerHTML =
+        "<h2>Mars Rover</h2>" +
+        "<img src='" + d.photos[0].img_src + "'>";
+    });
+}
